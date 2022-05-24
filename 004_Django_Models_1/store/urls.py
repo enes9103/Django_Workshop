0dd34efc1a9777d4336f29ed42d39267c018b6ad
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2022-05-24 12:14:05 +0300

    delete

--- 004_Django_Models_1/store/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-# from django.urls.conf import include      # bir üstteki gibi de bu şekilde de include import edilecilir.
-from .views import home
-
-urlpatterns = [
-    path('home/', home),
-]
