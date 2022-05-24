b4ed146f16e5181798ee5bd384db59e1dd48f95a
EnesUnlu <enes9103@gmail.com>
2022-05-24 12:15:53 +0300

    session 6

---
+++ 006_Views&Templates&StaticFiles/app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    print(request)
+    return HttpResponse('<h1>Hello World!</h1>')
+
+def special(request):
+    context = {
+        'title': 'clarusway',
+        'dict1': {'django': 'best framework'},
+        'my_list': [2, 3, 4]
+    }
+    return render(request, 'app/special.html', context)
