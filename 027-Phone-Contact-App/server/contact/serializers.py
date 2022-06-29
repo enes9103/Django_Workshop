bd2c190574cb718e4cacef0197a58d7193022178
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2022-06-29 23:12:01 +0300

    Delete 027-Phone-Contact-App directory

--- 027-Phone-Contact-App/server/contact/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import Contact
-from rest_framework import serializers
-
-class ContactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contact
-        fields = '__all__'
--- 027-Phone-Contact-App/server/contact/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ContactList, ContactRUD
-
-urlpatterns = [
-    path('contact/', ContactList.as_view()),
-    path('contact/<int:pk>/', ContactRUD.as_view())
-]
